4c97e349879d636cbb67183528555e0be79e71ce
Emmanoel <emmanoelsveiga@gmail.com>
2023-04-03 23:48:45 -0300

    :tada: Pronto

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,62 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { AiFillInstagram, AiFillTwitterCircle } from 'react-icons/ai'
 import './App.css'
+import moment from 'moment';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [countDown, setCountDown] = useState({} as any);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const eventTime = moment("2023-04-10 10:00:00");
+      const currentTime = moment();
+      const diffTime = eventTime.diff(currentTime);
+      const duration = moment.duration(diffTime);
+
+      const days = `0${Math.floor(duration.asDays())}`.slice(-2);
+      const hours = `0${duration.hours()}`.slice(-2);
+      const minutes = `0${duration.minutes()}`.slice(-2);
+      const seconds = `0${duration.seconds()}`.slice(-2);
+      setCountDown({ days, hours, minutes, seconds });
+    }, 1000)
+
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <img src="logo.png" alt="" />
+      <div className="countdown-list">
+        <div className="countdown">
+          <p>DIAS</p>
+          {countDown.days}
+        </div>
+        <div className="countdown">
+          <p>HORAS</p>
+          {countDown.hours}
+
+        </div>
+        <div className="countdown">
+          <p>MINUTOS</p>
+          {countDown.minutes}
+
+        </div>
+        <div className="countdown">
+          <p>SEGUNDOS</p>
+          {countDown.seconds}
+        </div>
+      </div>
+      <div className="icons">
+        <a href="https://www.instagram.com/atleticacompuesb/">
+        <AiFillInstagram />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://twitter.com/atleticacomp">
+        <AiFillTwitterCircle />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
