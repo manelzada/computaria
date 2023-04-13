52320aa70286ec3def1b6158b0b947c3ba78959b
Emmanoel <emmanoelsveiga@gmail.com>
2023-04-13 16:07:53 -0300

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,25 @@ import './App.css'
 import moment from 'moment';
 
 function App() {
-  const [countDown, setCountDown] = useState({} as any);
+  // const [countDown, setCountDown] = useState({} as any);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const eventTime = moment("2023-04-13 16:00:00");
-      const currentTime = moment();
-      const diffTime = eventTime.diff(currentTime);
-      const duration = moment.duration(diffTime);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     const eventTime = moment("2023-04-13 16:00:00");
+  //     const currentTime = moment();
+  //     const diffTime = eventTime.diff(currentTime);
+  //     const duration = moment.duration(diffTime);
 
-      const days = `0${Math.floor(duration.asDays())}`.slice(-2);
-      const hours = `0${duration.hours()}`.slice(-2);
-      const minutes = `0${duration.minutes()}`.slice(-2);
-      const seconds = `0${duration.seconds()}`.slice(-2);
-      setCountDown({ days, hours, minutes, seconds });
-    }, 1000)
+  //     const days = `0${Math.floor(duration.asDays())}`.slice(-2);
+  //     const hours = `0${duration.hours()}`.slice(-2);
+  //     const minutes = `0${duration.minutes()}`.slice(-2);
+  //     const seconds = `0${duration.seconds()}`.slice(-2);
+  //     setCountDown({ days, hours, minutes, seconds });
+  //   }, 1000)
 
 
-    return () => clearInterval(interval);
-  }, []);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return (
     <div className="App">
@@ -32,21 +32,21 @@ function App() {
       <div className="countdown-list">
         <div className="countdown">
           <p>DIAS</p>
-          {countDown.days}
+          00
         </div>
         <div className="countdown">
           <p>HORAS</p>
-          {countDown.hours}
+          00
 
         </div>
         <div className="countdown">
           <p>MINUTOS</p>
-          {countDown.minutes}
+          00
 
         </div>
         <div className="countdown">
           <p>SEGUNDOS</p>
-          {countDown.seconds}
+          00
         </div>
       </div>
       <div className="icons">
